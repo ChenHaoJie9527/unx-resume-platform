6f01c87de4a18c1ea77c976e5f1f8d99bca1c93e
chenhaojie <a18520145493@163.com>
2022-05-12 17:25:56 +0800

    配置eslint perttire

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from "path";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   root: path.resolve(__dirname, 'src'),
-  plugins: [vue()]
-})
+  plugins: [vue()],
+});
